110dad98ae5894fab823f0a280a9e434ce3632b8
Ricardo Oliveira <ricardodamaceno92@gmail.com>
2025-01-14 22:31:37 -0300

    fet: DependencyInjection

---
+++ DependencyInjection/DependencyInjectionConfig.cs
@@ -0,0 +1,19 @@
+﻿using ApiUserRegister.Repositories.Implementations;
+using ApiUserRegister.Repositories.Interfaces;
+using ApiUserRegister.Services.Implementations;
+using ApiUserRegister.Services.Interfaces;
+
+namespace ApiUserRegister.DependencyInjection
+{
+	public static class DependencyInjectionConfig
+	{
+		public static void RegisterServices(IServiceCollection services)
+		{
+			// Repositories
+			services.AddScoped<IUserRepository, UserRepository>();
+
+			// Services
+			services.AddScoped<IUserService, UserService>();
+		}
+	}
+}
