28d7af9443ce504b25a4e3418e9e434982f42841
Ricardo Oliveira <ricardodamaceno92@gmail.com>
2025-01-14 22:33:38 -0300

    fix: DependencyInjection

--- Program.cs
+++ Program.cs
@@ -1,9 +1,15 @@
+using ApiUserRegister.DependencyInjection;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 
 builder.Services.AddControllers();
-// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+
+// Configuração de serviços
+DependencyInjectionConfig.RegisterServices(builder.Services);
+
+
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
